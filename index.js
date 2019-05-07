6d66efb849d55ff2aea22594ad17dc48683a1c76
Nehal Gajjar <200388614@student.georgianc.on.ca>
2019-05-07 12:53:40 -0400

    COMP 2106 – Lab 1 – Rock Paper Scissors
    Write a JavaScript program that allows the user to play Rock Paper Scissors.
    
    The Program should ask the user if they want to choose rock, paper or scissors. A computer should be randomly assigned one of the three choices.
    
    Winning combinations are as follows:
    •	Rock beats Scissors
    •	Scissors beats Paper
    •	Paper beats Rock.
    The program should indicate who the winner was. This program should be written in vanilla javascript (No libraries).
    The code can either be
    A: intended for the browser, in which case an index.html file should be included which includes a script src to the program.
    B: intended for node, in which case the code should be able to be run by typing node (your_file).js in the terminal, and the program will run inside the terminal.
    The program should also handle the following issues:
    •	The player choosing an invalid option.
    •	Handling a tie (rethrow)
    
    BONUS:
    The program should ask the user if they want to play again, and when the user says no. It should print out the scores of wins/losses/ties.

--- index.js
+++ index.js
@@ -5,42 +5,113 @@ const rl = readline.createInterface({
   output: process.stdout // Similar to response in Node
 });
 let validOptions = ['rock', 'paper', 'scissors'];
+let evalResults = {
+    "Won" : 0,
+    "Ties" : 0,
+    "Loss" : 0,
+    "Score" : 0
+};
 
 const generateChoice = () => {
   const index = Math.floor(Math.random() * Math.floor(3));
   return validOptions[index];
 };
 
-rl.question(
+const getChoice = () => {
+    let choice = "";
+    rl.question(
   `Please choose one of the following:
 
-Rock
-Paper
-Scissors
-`,
-  userChoice => {
-    const choice = userChoice;
-    const computer = generateChoice();
+    Rock
+    Paper
+    Scissors
 
-    // Winning Conditions
-    // First is Tie
-    if (choice === computer) {
-      console.log('There was a tie');
-    } else if (
-      // User winning conditions
-      (choice === 'rock' && computer === 'scissors') ||
-      (choice === 'scissors' && computer === 'paper') ||
-      (choice === 'paper' && computer === 'rock')
-    ) {
-      console.log('Player won');
-    } else {
-      // Else computer won
-      console.log('Computer won');
+    `,
+    userChoice => {
+        var isValid = false;
+        choice = userChoice.toLowerCase();
+        if(isValid == false && validOptions.includes(choice)){
+            isValid = true;
+        }else{
+            isValid = false;
+        }
+        const computer = generateChoice();
+
+        if(isValid == true && choice.trim != undefined && computer != undefined){
+            // Winning Conditions
+            // First is Tie
+            if (choice === computer) {
+                console.log('There was a tie, time for tie breaker 1.2.3...');
+                evalResults.Ties += 1;
+                getChoice();
+            } else if (
+            // User winning conditions
+            (choice === 'rock' && computer === 'scissors') ||
+            (choice === 'scissors' && computer === 'paper') ||
+            (choice === 'paper' && computer === 'rock')
+            ) {
+                console.log(`
+                (Player) won over (Computer)
+                ${choice} > ${computer}
+                `);
+            console.log('Player won');
+            evalResults.Won += 1;
+            evalResults.Score += 10;
+            } else {
+            // Else computer won
+            console.log(`
+                (Player) lost over (Computer)
+                ${choice} < ${computer}
+            `);
+            console.log('Computer won');
+                evalResults.Loss += 1;
+                evalResults.Score -= 10;
+            }
+
+           let playAgain = () => 
+            { 
+                rl.question(
+                `
+                You seems to be intresting guy, let play again once ...? (Yes / No) 
+                `,
+                response => {
+                    let isOptionValid = false;
+                    const res = response.toLowerCase();
+                    if(isOptionValid == false && ["yes", "no"].includes(res)){
+                        isOptionValid = true;
+                    }else{
+                        isOptionValid = false;
+                    }
+                    if(isOptionValid){
+                        if(res == "yes"){
+                            getChoice();
+                        }else{
+console.log("\x1b[32m", `
+Player { Won: ${evalResults.Won}, Ties: ${evalResults.Ties}, Loss: ${evalResults.Loss} }
+Score: ${evalResults.Score}
+`);
+                            rl.close();
+                        console.log("\x1b[0m", "\nThank You! Made with Love by Nehal Gajjar.");
+                        }
+                    }else{
+                        console.log(`"${res}" is not Valid Option. Valid Options are "Yes" or "No".`);
+                        playAgain();
+                    }
+                 });
+            }
+            playAgain();
+        }else{
+            console.log(`
+            "${choice}" is not Valid Option. Valid Options are :
+            Rock
+            Paper
+            Scissors
+            `);
+            getChoice();
+        }
     }
+    );
+}
 
-    console.log(`Player chose: ${choice}
-    Computer chose: ${computer}`);
-
-    rl.close();
-  }
-);
+//Init
+getChoice();
