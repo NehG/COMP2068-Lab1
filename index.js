ce22698c08ecceac08478885802f7223e918ac93
Nehal Gajjar <200388614@student.georgianc.on.ca>
2019-05-06 21:21:34 -0400

    Lab1 Done

---
+++ index.js
@@ -0,0 +1,46 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin, // Similar to Request in Node
+  output: process.stdout // Similar to response in Node
+});
+let validOptions = ['rock', 'paper', 'scissors'];
+
+const generateChoice = () => {
+  const index = Math.floor(Math.random() * Math.floor(3));
+  return validOptions[index];
+};
+
+rl.question(
+  `Please choose one of the following:
+
+Rock
+Paper
+Scissors
+`,
+  userChoice => {
+    const choice = userChoice;
+    const computer = generateChoice();
+
+    // Winning Conditions
+    // First is Tie
+    if (choice === computer) {
+      console.log('There was a tie');
+    } else if (
+      // User winning conditions
+      (choice === 'rock' && computer === 'scissors') ||
+      (choice === 'scissors' && computer === 'paper') ||
+      (choice === 'paper' && computer === 'rock')
+    ) {
+      console.log('Player won');
+    } else {
+      // Else computer won
+      console.log('Computer won');
+    }
+
+    console.log(`Player chose: ${choice}
+    Computer chose: ${computer}`);
+
+    rl.close();
+  }
+);
